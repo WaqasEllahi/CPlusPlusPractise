02cc8283538a91463fde35518790548ef16a8bc1
WaqasEllahi <waqas.ellahi1@gmail.com>
2022-06-15 00:42:33 +0200

    Add google test project and rename projects names

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream>
+#include "simplemath.h"
+
+int main()
+{
+    std::cout << "Cubic of 10 = " << cubic(10) <<"\n";
+}
+
+
---
+++ HelloWorld/simplemath.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <cmath>
+
+double cubic(double d)
+{
+	return pow(d, 3);
+}
